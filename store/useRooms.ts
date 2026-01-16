454faa4e8177407f55b739e4a9c43dea5838f90b
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-16 20:07:42 +0530

    Add useRooms zustand store

---
+++ store/useRooms.ts
@@ -0,0 +1,24 @@
+import { Room } from "@/interfaces/Room";
+import { create } from "zustand";
+
+type RoomsStateType = {
+  rooms: Room[];
+  setRooms: (rooms: Room[]) => void;
+  addRoom: (room: Room) => void;
+  updateRoom: (roomData: Partial<Room>) => void;
+  deleteRoom: (roomId: string) => void;
+};
+
+export const useRooms = create<RoomsStateType>((set) => ({
+  rooms: [],
+  setRooms: (rooms) => set({ rooms }),
+  addRoom: (room) => set((s) => ({ rooms: [...s.rooms, room] })),
+  updateRoom: (roomData) =>
+    set((s) => ({
+      rooms: s.rooms.map((r) =>
+        r.$id === roomData.$id ? { ...r, ...roomData } : r
+      ),
+    })),
+  deleteRoom: (roomId) =>
+    set((s) => ({ rooms: s.rooms.filter((r) => r.$id !== roomId) })),
+}));
